94f508dd756886567e10f6a1b271f80f4e75c19c
Mitro09 <mpatrick94@libero.it>
2021-03-08 11:25:44 +0100

    aggiunta task1

--- case_study/tasklistArray/index.php
+++ case_study/tasklistArray/index.php
@@ -1,9 +1,24 @@
+<?php
+// Carico le dipendenze
+require "./lib/JSONReader.php";
+// Model(parte che gestisce i dati)
+$taskList = JSONReader('./dataset/TaskList.json');
+
+// Controller()
+
+
+
+?>
+
+
+<!-- VIEW/ intercetta azioni utente -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/style.css">
     <title>Task List</title>
 </head>
 <body>
@@ -11,12 +26,24 @@
         <input type="text" name="searchText">
         <button type="submit">CERCA</button>
     </form>
-    <ol>
-        <li class="tasklist-item tasklist-item-progress">latte <b>progress</b></li>
+    <ul>
+
+        <?php
+        foreach ($taskList as $key => $task){
+            $status = $task['status'];
+            $taskName = $task['taskName'];
+        ?>
+        
+        <li class="tasklist-item tasklist-item-<?= $status ?>">
+            <?= $taskName ?>
+            <b> <?= $status ?> </b>
+        </li>
+        <?php } ?>
+        <!--
         <li class="tasklist-item tasklist-item-done">uova <b>done</b></li>
-        <li class="tasklist-item tasklist-item-todo">uova <b>todo</b></li>
-    </ol>
-</body>
+        <li class="tasklist-item tasklist-item-todo">farina <b>todo</b></li>
+        -->
+    </ul>
 </html>
 
 
