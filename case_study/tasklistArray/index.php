e85617c4b849f16f836626b54bfaa56ab2594bad
Mitro09 <mpatrick94@libero.it>
2021-03-08 10:19:26 +0100

    salvataggio opzioni

--- case_study/tasklistArray/index.php
+++ case_study/tasklistArray/index.php
@@ -1,1 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task List</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="searchText">
+        <button type="submit">CERCA</button>
+    </form>
+    <ol>
+        <li class="tasklist-item tasklist-item-progress">latte <b>progress</b></li>
+        <li class="tasklist-item tasklist-item-done">uova <b>done</b></li>
+        <li class="tasklist-item tasklist-item-todo">uova <b>todo</b></li>
+    </ol>
+</body>
+</html>
+
+
